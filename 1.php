cedfc7061944b001e972402965d655bd4f15c6f0
xuan098 <43070488+xuan098@users.noreply.github.com>
2019-03-15 11:41:24 +0800

    Add files via upload

---
+++ 1.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>website</title>
+    <link href="style.css" type="text/css" rel="stylesheet"/>
+    <div id="head" align="right">头部组件：用户登录以及注册</div>
+    <h1>website</h1>
+<body bgcolor="#f0f8ff">
+<div id="quanju">
+<div id="top biaoqian" align="">动画 番剧 国创 科技 鬼畜 电影 电视 特摄 </div>
+<br>
+<div id="right biaoqian" align="centre">动 画<br>番 剧<br>国 创<br>科 技<br>鬼 畜<br>电 影<br>电 视<br>特 摄<br>
+</div>
+<div id="zhuyaoneirong">主要内容</div>
+<div id="dibuneirong">底部内容</div>
+</div>
+</body>
+</html>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xwr
+ * Date: 2019/3/13
+ * Time: 16:32
+ */
