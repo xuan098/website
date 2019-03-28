270dd2714f0d912101290a92acd446621343e639
xuan098 <43070488+xuan098@users.noreply.github.com>
2019-03-28 19:34:02 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,51 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>website</title>
+    <link href="style.css" type="text/css" rel="stylesheet"/>
+    <div id="head" align="right">
+        <a id="headuser" href="user/login.php">登录</a>
+        <a id="headuser" href="user/register.php">注册</a>
+    </div>
+    <h1>website</h1>
+<body bgcolor="#f0f8ff">
+<div id="quanju">
+    <nav id="top biaoqian" align="center">
+        <a id="top biaoqian1" href="index.php">首页</a>
+        <a id="top biaoqian1"href="">动画</a>
+        <a id="top biaoqian1"href="">番剧</a>
+        <a id="top biaoqian1"href="">国创</a>
+        <a id="top biaoqian1"href="">科技</a>
+        <a id="top biaoqian1"href="">鬼畜</a>
+        <a id="top biaoqian1"href="">电影</a>
+        <a id="top biaoqian1"href="">电视</a>
+        <a id="top biaoqian1" href="">特摄</a> </nav>
+    <br>
+    <aside id="right biaoqian" align="center">
+        <a href="index.php">首页</a><br/>
+        <a>动画</a><br/>
+        <a>番剧</a><br/>
+        <a>国创</a><br/>
+        <a>科技</a><br/>
+        <a>鬼畜</a><br/>
+        <a>电影</a><br/>
+        <a>电视</a><br/>
+        <a>特摄</a><br/>
+    </aside>
+    <div id="zhuyaoneirong">主要内容
+
+    </div>
+    <div id="dibuneirong">底部内容</div>
+</div>
+</body>
+</html>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xwr
+ * Date: 2019/3/13
+ * Time: 16:32
+ */
